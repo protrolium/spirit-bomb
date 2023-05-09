04fdabad429e0383aab8311fe9fc54cd1156e519
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-05-08 18:27:28 -0700

    added styles

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,2 +1,3 @@
-<?= $rockfrontend->renderIf("sections/default-page.latte", "template=default-page") ?>
+<!-- <?= $rockfrontend->renderIf("sections/default-page.latte", "template=default-page") ?> -->
 <?= $rockfrontend->renderIf("sections/news.latte", "template=news") ?>
+<?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
