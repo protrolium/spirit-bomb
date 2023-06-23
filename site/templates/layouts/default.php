3afcb1d5b77ecadd55ebdbfb0e39ef425ffbebe4
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-06-23 16:13:10 -0700

    label page

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,3 +1,4 @@
 <!-- <?= $rockfrontend->renderIf("sections/default-page.latte", "template=default-page") ?> -->
 <?= $rockfrontend->renderIf("sections/news.latte", "template=news") ?>
+<?= $rockfrontend->renderIf("sections/label.latte", "name=label") ?>
 <?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
