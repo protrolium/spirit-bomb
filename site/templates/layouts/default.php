b446acba8e6a20306ac13794211c914e47302827
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-06-26 19:15:26 -0700

    label-items template

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,4 +1,5 @@
 <!-- <?= $rockfrontend->renderIf("sections/default-page.latte", "template=default-page") ?> -->
 <?= $rockfrontend->renderIf("sections/news.latte", "template=news") ?>
 <?= $rockfrontend->renderIf("sections/label.latte", "name=label") ?>
+<?= $rockfrontend->renderIf("sections/label-item.latte", "template=label-item") ?>
 <?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
