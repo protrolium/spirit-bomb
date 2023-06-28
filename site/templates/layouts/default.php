4f64d7b50f2e4fe453801d2b0e1074654c60013a
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-06-27 20:18:25 -0700

    added editions template

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -2,4 +2,5 @@
 <?= $rockfrontend->renderIf("sections/news.latte", "template=news") ?>
 <?= $rockfrontend->renderIf("sections/label.latte", "name=label") ?>
 <?= $rockfrontend->renderIf("sections/label-item.latte", "template=label-item") ?>
+<?= $rockfrontend->renderIf("sections/editions.latte", "name=editions") ?>
 <?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
