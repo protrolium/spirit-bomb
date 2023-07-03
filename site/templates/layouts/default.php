521bf161c010a7350d27386674c3c8369a6f5798
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-07-03 16:39:53 -0700

    news page and editions, label, and home updates

--- site/templates/layouts/default.php
+++ site/templates/layouts/default.php
@@ -1,6 +1,8 @@
 <!-- <?= $rockfrontend->renderIf("sections/default-page.latte", "template=default-page") ?> -->
-<?= $rockfrontend->renderIf("sections/news.latte", "template=news") ?>
 <?= $rockfrontend->renderIf("sections/label.latte", "name=label") ?>
 <?= $rockfrontend->renderIf("sections/label-item.latte", "template=label-item") ?>
 <?= $rockfrontend->renderIf("sections/editions.latte", "name=editions") ?>
+<?= $rockfrontend->renderIf("sections/news.latte", "name=news") ?>
+<?= $rockfrontend->renderIf("sections/news-item.latte", "template=news") ?>
 <?= $rockfrontend->renderIf("sections/contact.latte", "name=contact") ?>
+
