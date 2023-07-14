ac8ec6442f69932beaccf9b8fdd35de153350213
Gavin Gamboa <gavin@strangeloop-studios.com>
2023-07-14 09:47:08 -0700

    first deploy to production

--- site/templates/_main.php
+++ site/templates/_main.php
@@ -40,8 +40,8 @@ $rockfrontend
 		<!-- make sure we get styling on mobile by setting meta viewport -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title><?php echo $page->title; ?></title>
-		<meta name="description" content="">
-		<meta name="keywords" content="">
+		<meta name="description" content="Spirit Bomb is a media label born from the combined contributions of Virtuals and Humans. It serves as a vehicle for creative collaboration between the IRL and Metaverse worlds as they begin to collide.">
+		<meta name="keywords" content="virtual being, virtual artist, spirit bomb, record label, media label, strangeloop studios, future, cybernetic, trap, hip hop, electro pop, glitch hop, doom, metal, artificial intelligence, metis, web3, nft, tokens, wiki, xen, lv4, det, izzi, antifragile, live streams, ai, cyberpunk, artwork, visuals, streamer, ar, skins, avatar">
 		<meta name="generator" content="ProcessWire">
 
 		<!-- fonts -->
